3f9d12358a348ad66ca2aa59e7df97700be92359
Matheus Augusto <mathieus_n.7@hotmail.com>
2023-04-14 19:42:23 -0300

    Add parameters 'a' and 'b' to 'add' and 'multiply'

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
-let add = (...args) => {
-    const sum = args.reduce((total, currentNumber) => {
-        return total += currentNumber;
-    }, 0);
+let add = (a, b, ...args) => {
+    let result = a + b;
+    
+    if (args.length > 0){
+        const sum = args.reduce((total, currentNumber) => {
+            return total += currentNumber;
+        }, result);
 
-    return sum;
+        return sum;
+
+    } else {
+        return result;
+    }
 };
 
 let subtract = (a, b, ...args) => {
@@ -21,12 +28,19 @@ let subtract = (a, b, ...args) => {
     }
 };
 
-let multiply = (...args) => {
-    const multi = args.reduce((total, currentNumber) => {
-        return total *= currentNumber;
-    }, 1);
+let multiply = (a, b, ...args) => {
+    let result = a * b;
 
-    return multi;
+    if (args.length > 0){
+        const multi = args.reduce((total, currentNumber) => {
+            return total *= currentNumber;
+        }, result);
+
+        return multi;
+
+    } else {
+        return result;
+    }
 };
 
 let divide = (a, b, ...args) => {
